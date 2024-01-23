a14291c378ea81667dff8de4daa803452f9ebe11
Prabhat Roy <prabhat00155@gmail.com>
2024-01-24 01:05:27 +0530

    Added flags category

--- lib/mapper.dart
+++ lib/mapper.dart
@@ -2,4 +2,5 @@ const Map<String, String> mapCategoryToAsset = {
   'Animals': 'assets/text/animals.json',
   'Birds': 'assets/text/birds.json',
   'Cities': 'assets/text/cities.json',
+  'Flags': 'assets/text/flags.json',
 };
