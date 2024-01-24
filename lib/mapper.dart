1bcdf4787f3852133d49d957dfe0bc8d6b4b5bba
Prabhat Roy <prabhat00155@gmail.com>
2024-01-24 23:54:05 +0530

    Added dishes category

--- lib/mapper.dart
+++ lib/mapper.dart
@@ -2,6 +2,7 @@ const Map<String, String> mapCategoryToAsset = {
   'Animals': 'assets/text/animals.json',
   'Birds': 'assets/text/birds.json',
   'Cities': 'assets/text/cities.json',
+  'Dishes': 'assets/text/dishes.json',
   'Flags': 'assets/text/flags.json',
   'Fruits': 'assets/text/fruits.json',
   'Sports': 'assets/text/sports.json',
