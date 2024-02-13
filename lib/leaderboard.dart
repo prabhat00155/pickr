7422bb1af4b9db52f3d129e681b3491c16fdb3a3
Prabhat Roy <prabhat00155@gmail.com>
2024-02-13 19:30:12 +0530

    Improved leaderboard details

--- lib/leaderboard.dart
+++ lib/leaderboard.dart
@@ -1,7 +1,8 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:country_flags/country_flags.dart';
-import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:pie_chart/pie_chart.dart';
 import 'package:random_avatar/random_avatar.dart';
 
 import 'constants.dart';
@@ -81,7 +82,7 @@ class _LeaderboardState extends State<Leaderboard> {
           side: const BorderSide(color: Colors.black, width: 1),
           borderRadius: BorderRadius.circular(10),
         ),
-        onTap: () => playerInfo(context, player),
+        onTap: () => playerInfo(context, player, rank),
         trailing: Text(
           'Rank: $rank',
           style: const TextStyle(
@@ -94,94 +95,140 @@ class _LeaderboardState extends State<Leaderboard> {
     );
   }
 
-  void playerInfo(BuildContext context, Player player) {
+  void playerInfo(BuildContext context, Player player, int rank) {
     int score = player.getScore();
     int highestScore = player.getHighestScore();
     showDialog(
       context: context,
       builder: (BuildContext context) {
         return AlertDialog(
-          content: SingleChildScrollView(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Row(
-                  children: [
-                    player.photoUrl == null
-                    ? RandomAvatar(player.avatar, height: 50, width: 50, trBackground: false)
-                    : CircleAvatar(
-                      radius: 30,
-                      backgroundImage: CachedNetworkImageProvider(
-                        player.photoUrl!,
-                      ),
-                    ),
-                    const SizedBox(width: 10),
-                    Flexible(
-                      flex: 1,
-                      child: Text(
-                        player.name ?? player.playerId,
-                        style: const TextStyle(
-                          fontSize: 18,
-                          color: Colors.black,
+          content: SizedBox(
+            width: MediaQuery.of(context).size.width * 0.8,
+            child: SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Row(
+                    children: [
+                      player.photoUrl == null
+                      ? RandomAvatar(player.avatar, height: 50, width: 50, trBackground: false)
+                      : CircleAvatar(
+                        radius: 30,
+                        backgroundImage: CachedNetworkImageProvider(
+                          player.photoUrl!,
                         ),
                       ),
-                    ),
-                    const SizedBox(width: 10),
-                    CountryFlag.fromCountryCode(
-                      player.countryCode,
-                      height: 36,
-                      width: 50,
-                      borderRadius: 8,
-                    ),
-                  ],
-                ),
-                const SizedBox(height: 10),
-                Text(
-                  'Total Score: $score',
-                  style: const TextStyle(
-                    fontSize: 18,
-                    color: Colors.black,
+                      const SizedBox(width: 10),
+                      Flexible(
+                        flex: 1,
+                        child: Text(
+                          player.name ?? player.playerId,
+                          style: const TextStyle(
+                            fontSize: 18,
+                            color: Colors.black,
+                          ),
+                        ),
+                      ),
+                      const SizedBox(width: 10),
+                      CountryFlag.fromCountryCode(
+                        player.countryCode,
+                        height: 36,
+                        width: 50,
+                        borderRadius: 8,
+                      ),
+                    ],
                   ),
-                ),
-                const SizedBox(height: 5),
-                Text(
-                  'Highest Score: $highestScore',
-                  style: const TextStyle(
-                    fontSize: 18,
-                    color: Colors.black,
-                  ),
-                ),
-                const SizedBox(height: 5),
-                const Divider(),
-                const Center(
-                  child: Text(
-                    'Badges',
-                    style: TextStyle(
+                  const SizedBox(height: 10),
+                  Text(
+                    'Rank: $rank',
+                    style: const TextStyle(
                       fontSize: 18,
                       color: Colors.black,
                     ),
-                  )
-                ),
-                const Divider(),
-                Wrap(
-                  spacing: 8.0,
-                  runSpacing: 4.0,
-                  children: [
-                    CircleAvatar(
-                      backgroundImage: AssetImage(levelToImage[player.level]!),
-                      radius: 50,
+                  ),
+                  const SizedBox(height: 5),
+                  Text(
+                    'Total Score: $score',
+                    style: const TextStyle(
+                      fontSize: 18,
+                      color: Colors.black,
                     ),
-                    ...(player.badges.map((badge) =>
+                  ),
+                  const SizedBox(height: 5),
+                  Text(
+                    'Highest Score: $highestScore',
+                    style: const TextStyle(
+                      fontSize: 18,
+                      color: Colors.black,
+                    ),
+                  ),
+                  const SizedBox(height: 5),
+                  const Divider(),
+                  const Center(
+                    child: Text(
+                      'Badges',
+                      style: TextStyle(
+                        fontSize: 18,
+                        color: Colors.black,
+                      ),
+                    ),
+                  ),
+                  const Divider(),
+                  Wrap(
+                    spacing: 8.0,
+                    runSpacing: 4.0,
+                    children: [
                       CircleAvatar(
-                        backgroundImage: AssetImage(badgeToImage[badge]!),
+                        backgroundImage: AssetImage(levelToImage[player.level]!),
                         radius: 50,
                       ),
-                    )),
-                  ],
-                ),
-                const Divider(),
-              ],
+                      ...(player.badges.map((badge) =>
+                        CircleAvatar(
+                          backgroundImage: AssetImage(badgeToImage[badge]!),
+                          radius: 50,
+                        ),
+                      )),
+                    ],
+                  ),
+                  const Divider(),
+                  const Center(
+                    child: Text(
+                      'Accuracy',
+                      style: TextStyle(
+                        fontSize: 18,
+                        color: Colors.black,
+                      ),
+                    )
+                  ),
+                  const Divider(),
+                  accuracyPieChart(context, player),
+                  const Divider(),
+                  const Center(
+                    child: Text(
+                      'Highest Scores',
+                      style: TextStyle(
+                        fontSize: 18,
+                        color: Colors.black,
+                      ),
+                    ),
+                  ),
+                  displayScores(player.perCategoryHighestScore),
+                  const Divider(),
+                  const Center(
+                    child: Text(
+                      'Total Scores',
+                      style: TextStyle(
+                        fontSize: 18,
+                        color: Colors.black,
+                      ),
+                    ),
+                  ),
+                  const Divider(),
+                  displayScores(player.perCategoryScores),
+                  const Divider(),
+                ],
+              ),
             ),
           ),
           actions: [
@@ -194,6 +241,104 @@ class _LeaderboardState extends State<Leaderboard> {
           ],
         );
       },
+    );
+  }
+
+  Widget accuracyPieChart(BuildContext context, Player player) {
+    return SingleChildScrollView(
+      scrollDirection: Axis.horizontal,
+      child: Row(
+        children: List.generate(categories.length, (index) {
+          String categoryName = categories[index].name;
+          String accuracyText;
+          int correct = player.perCategoryTotalCorrect[categoryName] ?? 0;
+          int attempts = player.perCategoryAttempts[categoryName] ?? 0;
+          double greenPercentage;
+          double redPercentage;
+
+          if (attempts == 0) {
+            accuracyText = 'N/A';
+            greenPercentage = 0;
+            redPercentage = 0;
+          } else {
+            double accuracy = 100.0 * correct / attempts;
+            greenPercentage = accuracy;
+            redPercentage = 100 - greenPercentage;
+            accuracyText = '${accuracy.toStringAsFixed(0)}%';
+          }
+
+          return Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              children: [
+                PieChart(
+                  dataMap: {
+                    'Green': greenPercentage,
+                    'Red': redPercentage,
+                  },
+                  colorList: const [
+                    Colors.green,
+                    Colors.red,
+                  ],
+                  chartType: ChartType.ring,
+                  chartRadius: MediaQuery.of(context).size.width / 7,
+                  centerText: accuracyText,
+                  legendOptions: const LegendOptions(
+                    showLegends: false,
+                  ),
+                  chartValuesOptions: const ChartValuesOptions(
+                    showChartValueBackground: false,
+                    showChartValues: false,
+                  ),
+                ),
+                const SizedBox(height: 10),
+                Text(categoryName),
+              ],
+            ),
+          );
+        }),
+      ),
+    );
+  }
+
+  Widget displayScores(scores) {
+    return SingleChildScrollView(
+      scrollDirection: Axis.horizontal,
+      child: Row(
+        children: List.generate(categories.length, (index) {
+          String categoryName = categories[index].name;
+          int score = scores[categoryName];
+          Color colour = score >= 50 ? Colors.green : Colors.red;
+
+          return Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              children: [
+                Container(
+                  width: 50,
+                  height: 50,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: colour,
+                  ),
+                  child: Center(
+                    child: Text(
+                      score.toString(),
+                      style: const TextStyle(
+                        color: Colors.white,
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 10),
+                Text(categoryName),
+              ],
+            ),
+          );
+        }),
+      ),
     );
   }
 }
