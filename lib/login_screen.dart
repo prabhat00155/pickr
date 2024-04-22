9798ef7a8ba900fa735255e1016e0590de6ac668
Prabhat Roy <prabhat00155@gmail.com>
2024-04-22 23:17:44 +0100

    Removed google signin option

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:google_sign_in/google_sign_in.dart';
 import 'package:internet_connection_checker_plus/internet_connection_checker_plus.dart';
 
 import 'constants.dart';
@@ -20,7 +19,6 @@ class _LoginScreenState extends State<LoginScreen> {
 
   _LoginScreenState() {
     mapper = {
-      'Google': _signInWithGoogle,
       'Guest': _continueAsGuest,
     };
   }
@@ -39,35 +37,6 @@ class _LoginScreenState extends State<LoginScreen> {
           : Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              _buildTile('Sign in with Google', 'assets/images/login/google.png', 'Google', context),
-              const SizedBox(height: 20),
-              Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 25.0),
-                  child: Row(
-                    children: [
-                      Expanded(
-                        child: Divider(
-                          thickness: 0.5,
-                          color: Colors.grey[400],
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 10.0),
-                        child: Text(
-                          'Or',
-                          style: TextStyle(color: Colors.grey[700]),
-                        ),
-                      ),
-                      Expanded(
-                        child: Divider(
-                          thickness: 0.5,
-                          color: Colors.grey[400],
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              const SizedBox(height: 20),
               _buildTile('Continue as Guest', const Icon(Icons.people), 'Guest', context),
             ],
           ),
@@ -107,53 +76,6 @@ class _LoginScreenState extends State<LoginScreen> {
         contentPadding: const EdgeInsets.only(left: 20.0),
       ),
     );
-  }
-
-  Future<void> _signInWithGoogle(BuildContext context) async {
-    setState(() {
-      _isSigningIn = true;
-    });
-
-    try {
-      final GoogleSignInAccount? googleSignInAccount = await GoogleSignIn().signIn();
-      final GoogleSignInAuthentication googleSignInAuthentication =
-          await googleSignInAccount!.authentication;
-      final AuthCredential credential = GoogleAuthProvider.credential(
-        accessToken: googleSignInAuthentication.accessToken,
-        idToken: googleSignInAuthentication.idToken,
-      );
-      await FirebaseAuth.instance.signInWithCredential(credential);
-      Navigator.of(context).pushReplacement(MaterialPageRoute(
-        builder: (context) => const HomeScreen(),
-      ));
-    } catch (error) {
-      logger('exception', {'title': 'LoginScreen', 'method': '_signInWithGoogle', 'file': 'login_screen', 'details': error.toString()});
-      showDialog(
-        context: context,
-        builder: (BuildContext context) {
-          return AlertDialog(
-            title: const Text('Login Info'),
-            content: const SingleChildScrollView(
-              child: Text(
-                'We cannot log you in using Google Account at the moment. Please use the "Continue as Guest" option.',
-              ),
-            ),
-            actions: [
-              TextButton(
-                onPressed: () {
-                  Navigator.of(context).pop();
-                },
-                child: const Text('Close'),
-              ),
-            ],
-          );
-        },
-      );
-    } finally {
-      setState(() {
-        _isSigningIn = false;
-      });
-    }
   }
 
   Future<void> _continueAsGuest(BuildContext context) async {
