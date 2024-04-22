93d44a62d5be1d10317fa1531e0a1ea55b2826ee
Prabhat Roy <prabhat00155@gmail.com>
2024-04-22 23:08:52 +0100

    Removed account linking option

--- lib/settings.dart
+++ lib/settings.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 
 import 'constants.dart';
-import 'login_screen.dart';
 import 'player.dart';
 
 class Settings extends StatefulWidget {
@@ -16,91 +14,17 @@ class Settings extends StatefulWidget {
 class SettingsState extends State<Settings> {
   String dropdownValue = languages.first;
   Player get player => widget.player;
-  final currentUser = FirebaseAuth.instance.currentUser!;
-  late Map<String, Function> mapper;
-
-  SettingsState() {
-    mapper = {
-      'Link with Google Account': accountLinking,
-    };
-  }
 
   @override
   Widget build(BuildContext context) {
-    List<Widget> settingsOptions = [
-      buildLanguage(context, 'Language'),
-    ];
-
-    if (currentUser.isAnonymous) {
-      settingsOptions.add(const SizedBox(height: 30));
-      settingsOptions.add(buildOption(context, 'Link with Google Account'));
-    }
-
     return Padding(
       padding: const EdgeInsets.all(16.0),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
-          ...settingsOptions,
+          buildLanguage(context, 'Language'),
         ],
       ),
-    );
-  }
-
-  GestureDetector buildOption(BuildContext context, String title) {
-    return GestureDetector(
-      onTap: () => mapper[title]?.call(context, title),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Text(
-            title,
-            style: TextStyle(
-              fontSize: 20,
-              fontWeight: FontWeight.w500,
-              color: Colors.grey[600],
-            ),
-          ),
-          const Icon(Icons.arrow_forward_ios, color: Colors.grey),
-        ],
-      ),
-    );
-  }
-
-  void accountLinking(BuildContext context, String title) async {
-    String response = await linkAccountWithGoogle();
-    User? user = FirebaseAuth.instance.currentUser;
-
-    if (user != null) {
-      for (var userInfo in user.providerData) {
-        if (userInfo.providerId == 'google.com') {
-          player.name = userInfo.displayName;
-          player.email = userInfo.email;
-          player.photoUrl = userInfo.photoURL;
-          break;
-        }
-      }
-    }
-    showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          title: Text(title),
-          content: SingleChildScrollView(
-            child: Text(
-              response,
-            ),
-          ),
-          actions: [
-            TextButton(
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-              child: const Text('Close'),
-            ),
-          ],
-        );
-      },
     );
   }
 
